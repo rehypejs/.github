db7188774251d9bb69a02c6843f5421296ca7f4e
Christian Murphy <christian.murphy.42@gmail.com>
2022-01-30 10:08:09 -0700

    docs: move sandbox examples into repository
    support both stackblitz and codesandbox

---
+++ sandbox-templates/rehype-debug/src/index.js
@@ -0,0 +1,26 @@
+// ⚠️ Important! Please make sure the dependencies are up to date.
+// On code sandbox, you can refresh them in the Dependencies section (left-bottom)
+// On stackblitz, you can open the package.json file, update the versions,
+// then run npm install in the stackblitz terminal
+
+import { rehype } from "rehype";
+
+const sourceHtml = `
+<html>
+  <head></head>
+  <body>
+    <h1>title</h1>
+    <p>lorem ipsum</p>
+  </body>
+</html>
+`;
+
+document.getElementById("source").textContent = sourceHtml;
+
+rehype()
+  // add any plugins here
+  .process(sourceHtml)
+  .then(
+    (file) => (document.getElementById("result").textContent = String(file))
+  )
+  .catch((err) => (document.getElementById("error").innerHTML = err));
