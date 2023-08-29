d9fe7fbe4f9e58840f90265c6196afda432cd55d
Christian Murphy <christian.murphy.42@gmail.com>
2023-08-29 09:50:52 -0700

    Update sandboxes to use vite and typescript

--- sandbox-templates/rehype-dom-debug/src/index.ts
+++ sandbox-templates/rehype-dom-debug/src/index.ts
@@ -19,18 +19,14 @@ const sourceHtml = `
 </html>
 `
 
-async function main() {
-  document.querySelector('#source').textContent = sourceHtml
+try {
+  document.querySelector('#source')!.textContent = sourceHtml
 
-  rehypeDom()
+  const file = await rehypeDom()
     .use({settings: {fragment: false}})
     // Add any plugins here
     .process(sourceHtml)
-    .then((file) => {
-      document.querySelector('#result').textContent = String(file)
-    })
+  document.querySelector('#result')!.textContent = String(file)
+} catch (error) {
+  document.querySelector('#error')!.textContent = String(error)
 }
-
-main().catch((error) => {
-  document.querySelector('#error').textContent = error
-})
