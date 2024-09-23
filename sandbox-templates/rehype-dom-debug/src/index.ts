9e92d0a62357853e4c625e10e84c0c7f68a2a1a4
Titus Wormer <tituswormer@gmail.com>
2024-09-23 16:51:39 +0200

    Refactor sandboxes

--- sandbox-templates/rehype-dom-debug/src/index.ts
+++ sandbox-templates/rehype-dom-debug/src/index.ts
@@ -1,32 +1,39 @@
 // ⚠️ Important! Please make sure the dependencies are up to date.
-// On code sandbox, you can refresh them in the Dependencies section (left-bottom)
+// On code sandbox, you can refresh them in the Dependencies section (left-bottom).
 // On stackblitz, you can open the package.json file, update the versions,
-// then run npm install in the stackblitz terminal
+// then run npm install in the stackblitz terminal.
 
 import {rehypeDom} from 'rehype-dom'
 
-const sourceHtml = `
+const html = `
 <!doctype html>
-<html>
+<html lang="en">
   <head>
-    <meta charset="utf8">
-    <title>sandbox document</title>
+    <meta charset="utf-8">
+    <title>Saturn</title>
   </head>
   <body>
-    <h1>title</h1>
-    <p>lorem ipsum</p>
+    <h1>Saturn</h1>
+    <p>Saturn is a gas giant composed predominantly of hydrogen and helium.</p>
   </body>
 </html>
 `
 
+const $error = document.querySelector('#error')!
+const $result = document.querySelector('#result')!
+const $source = document.querySelector('#source')!
+
 try {
-  document.querySelector('#source')!.textContent = sourceHtml
+  $source.textContent = html
 
   const file = await rehypeDom()
     .use({settings: {fragment: false}})
     // Add any plugins here
-    .process(sourceHtml)
-  document.querySelector('#result')!.textContent = String(file)
+    .process(html)
+
+  $error.textContent = ''
+  $result.textContent = String(file)
 } catch (error) {
-  document.querySelector('#error')!.textContent = String(error)
+  $error.textContent = String(error)
+  $result.textContent = ''
 }
